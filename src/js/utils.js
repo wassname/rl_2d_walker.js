dabfb64ac6f6d1bea502097e7d260ce068ce06da
wassname <wassname@users.noreply.github.com>
2018-12-01 16:54:16 +0800

    make class

--- src/js/utils.js
+++ src/js/utils.js
@@ -4,4 +4,29 @@ function deg2rad(deg) {
   return deg / 180 * Math.PI
 }
 
-module.exports = {deg2rad, randf}
+
+class MovingAverage{
+  constructor(N) {
+    this.N = N
+    this.buffer = []
+  }
+  add(val) {
+    if (this.buffer.length > this.N) this.buffer.splice(0, 1)
+    this.buffer.push(val)
+  }
+  mean() { 
+    return mean(this.buffer)
+  }
+}
+
+
+function mean(array){
+  if (array.length == 0)
+      return null;
+  var sum = array.reduce(function(a, b) { return a + b; });
+  var avg = sum / array.length;
+  return avg;
+}
+
+
+module.exports = {deg2rad, randf, MovingAverage, mean}
